39bbb084737c9851db64a42c1a768869fbdfe215
KaungMinKhant <kaungminkhant.edka@gmail.com>
2023-10-19 14:25:18 +0630

    newVersion

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+        <div className='flex items-center   h-screen'>
+           <div className='ml-40 p-4 font-sans subpixel-antialiased text-white '>
+                <h3 className='text-6xl font-semibold' style={{ fontFamily: "'Poppins', sans-serif"}}>Kaung Min Khant</h3>
+           </div>
+        </div>
+  )
+}
