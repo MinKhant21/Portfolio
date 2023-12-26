d4af913819b93f8f4ff2f036993fe68efd1b5a6b
KaungMinKhant <kaungminkhant.edka@gmail.com>
2023-12-26 19:59:52 +0630

    mobile contact

--- src/pages/Contant.jsx
+++ src/pages/Contant.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Hero from '../assets/images/kaungminkhant (1).png'
 export default function Contant() {
   return (
-    <div className='md:flex justify-around container  mx-auto'>
-          <div className=' flex justify-center items-center '>
+    <div className='container   md:flex justify-center  items-center mx-auto h-screen'>
+          <div className=' flex justify-center  items-center'>
                <img src={Hero} alt="" className='w-[400px] h-[400px] ' />
           </div>
-          <div className=' w-3/4 mx-10 md:w-1/2 '>
-               <h1 className=' text-3xl font-sans font-semibold my-10'>Contact Us</h1>
+          <div className=' w-3/4 mx-10 md:w-1/2 mb-32'>
+               
                <form class=" mx-auto">
+               <h1 className=' text-3xl font-sans font-semibold my-10'>Contact Us</h1>
                <div class="relative z-0 w-full mb-5 group">
                     <input type="email" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                     <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
