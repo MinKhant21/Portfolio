d95284ef22d5954a9cd3ad8f13073d90b728eb1e
KaungMinKhant <kaungminkhant.edka@gmail.com>
2024-01-03 09:44:53 +0630

    footer ui bug fixed

--- src/pages/Layout/Footer.jsx
+++ src/pages/Layout/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-     <footer class=" h-20 md:h-10 dark:bg-gray-900 mt-10 container mx-auto">
+     <footer class=" md:h-10 dark:bg-gray-900 mt-10 container mx-auto">
      <div class="container flex flex-col items-center justify-between p-6 mx-auto space-y-4 sm:space-y-0 sm:flex-row">
          <a href="#">
              {/* <img class="w-auto h-7" src="https://merakiui.com/images/full-logo.svg" alt=""/> */}
